fbbeac4bedce7c711e6d6136f900cadc33380615
Spica-Suzuki <76227757+Spica-Suzuki@users.noreply.github.com>
2021-09-10 11:05:16 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 pacman -S sudo alsa alsa-utils alsa-plugins pulseaudio chromium nomacs gimp terminator feh discord nautilus xorg xorg-xinit xorg-server alsamixer i3-gaps i3blocks i3lock i3status dmenu zsh vlc flameshot
 
+echo "exec i3" >> /etc/X11/xinit/xinitrc
+
 useradd -m susu
 mkdir -p /home/susu/wallpaper
 mkdir -p /home/susu/.config/i3
@@ -12,7 +14,6 @@ cp ./susu/.profile /home/susu/.profile
 chown -R susu:susu /home/susu
 
 cat ./susu/sudoers >> /etc/sudoers
-echo "exec i3" >> /etc/X11/xinit/xinitrc
 chown susu:susu /opt
 
 git clone https://aur.archlinux.org/yay.git /home/susu/
