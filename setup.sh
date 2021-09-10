48c6928e759d5c430e3dc226f60df91bed05aa6d
Spica-Suzuki <76227757+Spica-Suzuki@users.noreply.github.com>
2021-09-10 08:40:21 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
 #!/bin/ash
 
-echo "========== step1  ================"
-./step1.sh
-echo "========== step2  ================"
-./step2.sh
-echo "========== step3  ================"
-./step3.sh
+echo "========== yukleniyor  ================"
+./install.sh
