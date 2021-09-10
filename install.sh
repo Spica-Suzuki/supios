5a96fab5e032890637f7c8b56ca922a880b61de2
Spica-Suzuki <76227757+Spica-Suzuki@users.noreply.github.com>
2021-09-10 11:04:28 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,20 +2,17 @@
 
 pacman -S sudo alsa alsa-utils alsa-plugins pulseaudio chromium nomacs gimp terminator feh discord nautilus xorg xorg-xinit xorg-server alsamixer i3-gaps i3blocks i3lock i3status dmenu zsh vlc flameshot
 
-# add user
 useradd -m susu
 mkdir -p /home/susu/wallpaper
 mkdir -p /home/susu/.config/i3
 
-# user setup susu
 cp ./susu/wallpaper/air.jpg /home/susu/wallpaper/air.jpg
 cp ./susu/.config/i3/config /home/susu/.config/i3/config
 cp ./susu/.profile /home/susu/.profile
 chown -R susu:susu /home/susu
 
-# add susu to sudoers
 cat ./susu/sudoers >> /etc/sudoers
-
+echo "exec i3" >> /etc/X11/xinit/xinitrc
 chown susu:susu /opt
 
 git clone https://aur.archlinux.org/yay.git /home/susu/
@@ -23,4 +20,4 @@ chown -R  susu:susu /home/susu/yay
 su susu
 makepkg -si /home/susu/yay
 yay -S bumblebee-status
-echo "exec i3" >> /etc/X11/xinit/xinitrc
+
