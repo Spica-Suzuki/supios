657f4186a950c48d1e7dc507bf3fac6420387664
Spica-Suzuki <76227757+Spica-Suzuki@users.noreply.github.com>
2021-09-10 09:17:36 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pacman -S chromium nomacs gimp terminator feh discord nautilus i3-gaps i3blocks i3lock i3status dmenu zsh git vlc
+pacman -S chromium nomacs gimp terminator feh discord nautilus i3-gaps i3blocks i3lock i3status dmenu zsh vlc
